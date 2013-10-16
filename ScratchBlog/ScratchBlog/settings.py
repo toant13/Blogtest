755186913a85e199d8f8c47e26f278b7915b8512
toant <toant@128.49.162.189>
2013-10-16 11:47:04 -0700

    Added childen templates

--- ScratchBlog/ScratchBlog/settings.py
+++ ScratchBlog/ScratchBlog/settings.py
@@ -111,7 +111,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "C:/Users/toant/Documents/workspace/django/ScratchBlog/templates"
+    "C:/Users/toant/git/Blog/ScratchBlog/templates/"
 )
 
 INSTALLED_APPS = (
--- ScratchBlog/blogapp/views.py
+++ ScratchBlog/blogapp/views.py
@@ -6,4 +6,4 @@ from blogapp.models import Post
 def index(request):
     latest_blog_list = Post.objects.order_by('-created')[:5]
     context = {'latest_blog_list': latest_blog_list}
-    return render (request, 'blogmain/index.html', context)
+    return render (request, 'blogmain/content.html', context)
