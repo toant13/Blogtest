f9170421816f279cd706adc5f123db4ce5cd618b
toant <toant@128.49.162.189>
2013-10-16 09:38:46 -0700

    Changed db path

--- ScratchBlog/ScratchBlog/settings.py
+++ ScratchBlog/ScratchBlog/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:\\Users\\toant\\Documents\\workspace\\django\\ScratchBlog\\sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:\\Users\\toant\\git\\Blog\\ScratchBlog\\sqlite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
